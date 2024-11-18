c4ea0711177bfffdb1913bc3f2f11a8d4b6fe614
Yasmeen Gad <gadyasmeen335@gmail.com>
2024-11-18 18:44:43 +0200

    feat: call get init route method in initial route

--- lib/flowery_ecommerce.dart
+++ lib/flowery_ecommerce.dart
@@ -18,7 +18,7 @@ class FloweryEcommerce extends StatelessWidget {
       minTextAdapt: true,
       splitScreenMode: true,
       builder: (_, child) => MaterialApp(
-        initialRoute: AppRoutes.login,
+        initialRoute: _getInitialRoute(),
         debugShowCheckedModeBanner: false,
         onGenerateRoute: AppRoutes.onGenerateRoute,
         navigatorKey: getIt<GlobalKey<NavigatorState>>(),
