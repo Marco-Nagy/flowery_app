603c868c74d3232e7ce41008eace42804344b50b
Yasmeen Gad <gadyasmeen335@gmail.com>
2024-11-21 17:04:13 +0200

    feat: create states of get all categories view model

--- lib/features/categories/presentation/categories/viewModel/categories_view_model_cubit.dart
+++ lib/features/categories/presentation/categories/viewModel/categories_view_model_cubit.dart
@@ -1,6 +1,9 @@
 import 'package:bloc/bloc.dart';
 import 'package:meta/meta.dart';
 
+import '../../../../../core/networking/error/error_model.dart';
+import '../../../domain/entities/response/get_all_categories_response_entity.dart';
+
 part 'categories_view_model_state.dart';
 
 class CategoriesViewModelCubit extends Cubit<CategoriesViewModelState> {
--- lib/features/categories/presentation/categories/viewModel/categories_view_model_state.dart
+++ lib/features/categories/presentation/categories/viewModel/categories_view_model_state.dart
@@ -4,3 +4,17 @@ part of 'categories_view_model_cubit.dart';
 sealed class CategoriesViewModelState {}
 
 final class CategoriesViewModelInitial extends CategoriesViewModelState {}
+
+final class GetCategoriesViewModelLoading extends CategoriesViewModelState {}
+
+final class GetCategoriesViewModelSuccess extends CategoriesViewModelState {
+  final GetAllCategoriesResponseEntity categories;
+
+  GetCategoriesViewModelSuccess({required this.categories});
+}
+
+final class GetCategoriesViewModelError extends CategoriesViewModelState {
+  final ErrorModel error;
+
+  GetCategoriesViewModelError({required this.error});
+}
