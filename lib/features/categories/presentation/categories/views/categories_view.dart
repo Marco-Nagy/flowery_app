25c105cc5fd39659765a266779f2be000a92ddd6
Yasmeen Gad <gadyasmeen335@gmail.com>
2024-11-21 17:35:12 +0200

    feat: create categories view feat: add categories view route in app routes

---
+++ lib/features/categories/presentation/categories/views/categories_view.dart
@@ -0,0 +1,11 @@
+import 'package:flowery_e_commerce/core/utils/widgets/base/base_view.dart';
+import 'package:flutter/material.dart';
+
+class CategoriesView extends StatelessWidget {
+  const CategoriesView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return BaseView(child: Container());
+  }
+}
