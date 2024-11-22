b1c607b104f74651761e47180b544bb1e90d679c
Haidy <136759109+haidyhesham95@users.noreply.github.com>
2024-11-22 07:13:39 +0200

    update files

---
+++ lib/core/networking/api/api_manager.g.dart
@@ -0,0 +1,189 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'api_manager.dart';
+
+// **************************************************************************
+// RetrofitGenerator
+// **************************************************************************
+
+// ignore_for_file: unnecessary_brace_in_string_interps,no_leading_underscores_for_local_identifiers,unused_element,unnecessary_string_interpolations
+
+class _ApiManager implements ApiManager {
+  _ApiManager(
+    this._dio, {
+    this.baseUrl,
+    this.errorLogger,
+  }) {
+    baseUrl ??= 'https://flower.elevateegy.com/';
+  }
+
+  final Dio _dio;
+
+  String? baseUrl;
+
+  final ParseErrorLogger? errorLogger;
+
+  @override
+  Future<LoginResponseDto> login(LoginRequestDto request) async {
+    final _extra = <String, dynamic>{};
+    final queryParameters = <String, dynamic>{};
+    final _headers = <String, dynamic>{};
+    final _data = <String, dynamic>{};
+    _data.addAll(request.toJson());
+    final _options = _setStreamType<LoginResponseDto>(Options(
+      method: 'POST',
+      headers: _headers,
+      extra: _extra,
+    )
+        .compose(
+          _dio.options,
+          'api/v1/auth/signin',
+          queryParameters: queryParameters,
+          data: _data,
+        )
+        .copyWith(
+            baseUrl: _combineBaseUrls(
+          _dio.options.baseUrl,
+          baseUrl,
+        )));
+    final _result = await _dio.fetch<Map<String, dynamic>>(_options);
+    late LoginResponseDto _value;
+    try {
+      _value = LoginResponseDto.fromJson(_result.data!);
+    } on Object catch (e, s) {
+      errorLogger?.logError(e, s, _options);
+      rethrow;
+    }
+    return _value;
+  }
+
+  @override
+  Future<SignUpResponseDto> signUp(SignUpRequestDto request) async {
+    final _extra = <String, dynamic>{};
+    final queryParameters = <String, dynamic>{};
+    final _headers = <String, dynamic>{};
+    final _data = <String, dynamic>{};
+    _data.addAll(request.toJson());
+    final _options = _setStreamType<SignUpResponseDto>(Options(
+      method: 'POST',
+      headers: _headers,
+      extra: _extra,
+    )
+        .compose(
+          _dio.options,
+          'api/v1/auth/signup',
+          queryParameters: queryParameters,
+          data: _data,
+        )
+        .copyWith(
+            baseUrl: _combineBaseUrls(
+          _dio.options.baseUrl,
+          baseUrl,
+        )));
+    final _result = await _dio.fetch<Map<String, dynamic>>(_options);
+    late SignUpResponseDto _value;
+    try {
+      _value = SignUpResponseDto.fromJson(_result.data!);
+    } on Object catch (e, s) {
+      errorLogger?.logError(e, s, _options);
+      rethrow;
+    }
+    return _value;
+  }
+
+  @override
+  Future<GenericResponseDto> getGenericProduct(String resourceName) async {
+    final _extra = <String, dynamic>{};
+    final queryParameters = <String, dynamic>{};
+    final _headers = <String, dynamic>{};
+    const Map<String, dynamic>? _data = null;
+    final _options = _setStreamType<GenericResponseDto>(Options(
+      method: 'GET',
+      headers: _headers,
+      extra: _extra,
+    )
+        .compose(
+          _dio.options,
+          'api/v1/${resourceName}',
+          queryParameters: queryParameters,
+          data: _data,
+        )
+        .copyWith(
+            baseUrl: _combineBaseUrls(
+          _dio.options.baseUrl,
+          baseUrl,
+        )));
+    final _result = await _dio.fetch<Map<String, dynamic>>(_options);
+    late GenericResponseDto _value;
+    try {
+      _value = GenericResponseDto.fromJson(_result.data!);
+    } on Object catch (e, s) {
+      errorLogger?.logError(e, s, _options);
+      rethrow;
+    }
+    return _value;
+  }
+
+  @override
+  Future<GetAllCategoriesResponseDto> getAllCategories() async {
+    final _extra = <String, dynamic>{};
+    final queryParameters = <String, dynamic>{};
+    final _headers = <String, dynamic>{};
+    const Map<String, dynamic>? _data = null;
+    final _options = _setStreamType<GetAllCategoriesResponseDto>(Options(
+      method: 'GET',
+      headers: _headers,
+      extra: _extra,
+    )
+        .compose(
+          _dio.options,
+          'api/v1/categories',
+          queryParameters: queryParameters,
+          data: _data,
+        )
+        .copyWith(
+            baseUrl: _combineBaseUrls(
+          _dio.options.baseUrl,
+          baseUrl,
+        )));
+    final _result = await _dio.fetch<Map<String, dynamic>>(_options);
+    late GetAllCategoriesResponseDto _value;
+    try {
+      _value = GetAllCategoriesResponseDto.fromJson(_result.data!);
+    } on Object catch (e, s) {
+      errorLogger?.logError(e, s, _options);
+      rethrow;
+    }
+    return _value;
+  }
+
+  RequestOptions _setStreamType<T>(RequestOptions requestOptions) {
+    if (T != dynamic &&
+        !(requestOptions.responseType == ResponseType.bytes ||
+            requestOptions.responseType == ResponseType.stream)) {
+      if (T == String) {
+        requestOptions.responseType = ResponseType.plain;
+      } else {
+        requestOptions.responseType = ResponseType.json;
+      }
+    }
+    return requestOptions;
+  }
+
+  String _combineBaseUrls(
+    String dioBaseUrl,
+    String? baseUrl,
+  ) {
+    if (baseUrl == null || baseUrl.trim().isEmpty) {
+      return dioBaseUrl;
+    }
+
+    final url = Uri.parse(baseUrl);
+
+    if (url.isAbsolute) {
+      return url.toString();
+    }
+
+    return Uri.parse(dioBaseUrl).resolveUri(url).toString();
+  }
+}
---
+++ lib/di/di.config.dart
@@ -0,0 +1,114 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+// **************************************************************************
+// InjectableConfigGenerator
+// **************************************************************************
+
+// ignore_for_file: type=lint
+// coverage:ignore-file
+
+// ignore_for_file: no_leading_underscores_for_library_prefixes
+import 'package:dio/dio.dart' as _i361;
+import 'package:flutter/material.dart' as _i409;
+import 'package:get_it/get_it.dart' as _i174;
+import 'package:injectable/injectable.dart' as _i526;
+
+import '../core/networking/api/api_manager.dart' as _i282;
+import '../core/networking/common/regestet_context_module.dart' as _i125;
+import '../core/networking/network_factory.dart' as _i377;
+import '../features/auth/data/data_sources/contracts/auth_online_data_source.dart'
+    as _i901;
+import '../features/auth/data/data_sources/contracts/offline_data_source.dart'
+    as _i345;
+import '../features/auth/data/data_sources/impl/auth_offline_data_source_impl.dart'
+    as _i1036;
+import '../features/auth/data/data_sources/impl/auth_online_data_source_impl.dart'
+    as _i326;
+import '../features/auth/data/repositories/auth_repo_impl.dart' as _i990;
+import '../features/auth/domain/contracts/auth_repo.dart' as _i665;
+import '../features/auth/domain/use_cases/login_use_case.dart' as _i496;
+import '../features/auth/domain/use_cases/signup_use_case.dart' as _i853;
+import '../features/auth/presentation/login/viewModel/login_view_model_cubit.dart'
+    as _i690;
+import '../features/auth/presentation/signup/view_model/signup_view_model_cubit.dart'
+    as _i508;
+import '../features/categories/data/data_sources/contracts/categories_online_data_source.dart'
+    as _i518;
+import '../features/categories/data/data_sources/impl/categories_online_data_source_impl.dart'
+    as _i1059;
+import '../features/categories/data/repositories/categories_repo_impl.dart'
+    as _i620;
+import '../features/categories/domain/repositories/categories_repo.dart'
+    as _i590;
+import '../features/categories/domain/use_cases/categories_use_case.dart'
+    as _i777;
+import '../features/categories/presentation/categories/viewModel/categories_view_model_cubit.dart'
+    as _i80;
+import '../features/generic/data/data_sources/contracts/generic_online_data_source.dart'
+    as _i211;
+import '../features/generic/data/data_sources/impl/generic_online_data_source_impl.dart'
+    as _i854;
+import '../features/generic/data/repositories/generic_repo_impl.dart' as _i284;
+import '../features/generic/domain/contracts/generic_repo.dart' as _i565;
+import '../features/generic/domain/use_cases/generic_use_case.dart' as _i559;
+import '../features/generic/presentation/view_model/generic_view_model_cubit.dart'
+    as _i1070;
+
+extension GetItInjectableX on _i174.GetIt {
+// initializes the registration of main-scope dependencies inside of GetIt
+  _i174.GetIt init({
+    String? environment,
+    _i526.EnvironmentFilter? environmentFilter,
+  }) {
+    final gh = _i526.GetItHelper(
+      this,
+      environment,
+      environmentFilter,
+    );
+    final networkFactory = _$NetworkFactory();
+    final registerModule = _$RegisterModule();
+    gh.factory<_i361.LogInterceptor>(
+        () => networkFactory.providerInterceptor());
+    gh.singleton<_i409.GlobalKey<_i409.NavigatorState>>(
+        () => registerModule.navigatorKey);
+    gh.lazySingleton<_i361.Dio>(() => networkFactory.provideDio());
+    gh.singleton<_i282.ApiManager>(() => _i282.ApiManager(gh<_i361.Dio>()));
+    gh.factory<_i901.AuthOnlineDataSource>(
+        () => _i326.AuthOnlineDataSourceImpl(gh<_i282.ApiManager>()));
+    gh.factory<_i345.OfflineDataSource>(
+        () => _i1036.OfflineDataSourceImplementation());
+    gh.factory<_i211.GenericOnlineDataSource>(
+        () => _i854.GenericOnlineDataSourceImpl(gh<_i282.ApiManager>()));
+    gh.factory<_i518.CategoriesOnlineDataSource>(() =>
+        _i1059.CategoriesOnlineDataSourceImplementation(
+            gh<_i282.ApiManager>()));
+    gh.factory<_i665.AuthRepository>(
+        () => _i990.AuthRepositoryImpl(gh<_i901.AuthOnlineDataSource>()));
+    gh.factory<_i565.GenericRepo>(
+        () => _i284.GenericRepositoryImpl(gh<_i211.GenericOnlineDataSource>()));
+    gh.factory<_i496.LoginUseCase>(
+        () => _i496.LoginUseCase(gh<_i665.AuthRepository>()));
+    gh.factory<_i853.SignUpUseCase>(
+        () => _i853.SignUpUseCase(gh<_i665.AuthRepository>()));
+    gh.factory<_i590.CategoriesRepository>(() =>
+        _i620.CategoriesRepositoryImplementation(
+            gh<_i518.CategoriesOnlineDataSource>()));
+    gh.factory<_i690.LoginViewModel>(
+        () => _i690.LoginViewModel(gh<_i496.LoginUseCase>()));
+    gh.factory<_i508.SignUpViewModel>(
+        () => _i508.SignUpViewModel(gh<_i853.SignUpUseCase>()));
+    gh.factory<_i559.GenericUseCase>(
+        () => _i559.GenericUseCase(gh<_i565.GenericRepo>()));
+    gh.factory<_i777.CategoriesUseCase>(
+        () => _i777.CategoriesUseCase(gh<_i590.CategoriesRepository>()));
+    gh.factory<_i80.CategoriesViewModelCubit>(
+        () => _i80.CategoriesViewModelCubit(gh<_i777.CategoriesUseCase>()));
+    gh.factory<_i1070.GenericViewModelCubit>(
+        () => _i1070.GenericViewModelCubit(gh<_i559.GenericUseCase>()));
+    return this;
+  }
+}
+
+class _$NetworkFactory extends _i377.NetworkFactory {}
+
+class _$RegisterModule extends _i125.RegisterModule {}
