065f43e86238058221f17a821628f320cd945237
Marco Nagy <marconbishay@gmail.com>
2024-11-25 21:56:42 +0200

    handle App Loader widget with all screens

--- lib/features/home_screen/presentation/widgets/custom_occasion_list.dart
+++ lib/features/home_screen/presentation/widgets/custom_occasion_list.dart
@@ -1,5 +1,7 @@
 import 'package:flowery_e_commerce/core/styles/colors/my_colors.dart';
 import 'package:flowery_e_commerce/core/styles/fonts/my_fonts.dart';
+import 'package:flowery_e_commerce/core/utils/widgets/base/app_loader.dart';
+import 'package:flowery_e_commerce/core/utils/widgets/base/snack_bar.dart';
 import 'package:flowery_e_commerce/features/generic/presentation/widgets/cached_network_widget.dart';
 import 'package:flowery_e_commerce/features/home_screen/presentation/home_cubit/occasions_cubit/occasions_cubit.dart';
 import 'package:flowery_e_commerce/features/home_screen/presentation/home_cubit/occasions_cubit/occasions_states.dart';
@@ -21,35 +23,46 @@ class CustomOccasionListState extends State<CustomOccasionList> {
       height: 230.h,
       child: BlocBuilder<OccasionsCubit, OccasionsStates>(
         builder: (context, state) {
-          if(state is GetOccasionsSuccessState){
-            return ListView.builder(
-              itemCount: state.occasions.length,
-              scrollDirection: Axis.horizontal,
-              itemBuilder: (context, index) {
-                return Container(
-                  margin: EdgeInsets.all(5.sp),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                    SizedBox(
-                    width: 161.w,
-                    height: 181.h,
-                    child: CachedNetworkWidget(
-                      imageUrl:
-                        state.occasions[index].image,
-                      ),
+          switch (state) {
+
+            case GetOccasionsLoadingState():
+              AppLoader();
+            case GetOccasionsSuccessState():
+              return ListView.builder(
+                itemCount: state.occasions.length,
+                scrollDirection: Axis.horizontal,
+                itemBuilder: (context, index) {
+                  return Container(
+                    margin: EdgeInsets.all(5.sp),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        SizedBox(
+                          width: 161.w,
+                          height: 181.h,
+                          child: CachedNetworkWidget(
+                            imageUrl: state.occasions[index].image,
+                          ),
+                        ),
+                        SizedBox(
+                          height: 5.h,
+                        ),
+                        Text(state.occasions[index].name,
+                            style: MyFonts.styleMedium500_14
+                                .copyWith(color: MyColors.blackBase)),
+                      ],
                     ),
-                      SizedBox(height: 5.h,),
-                      Text(state.occasions[index].name, style: MyFonts.styleMedium500_14.copyWith(
-                          color: MyColors.blackBase)),
-                    ],
-                  ),
-                );
-              },
-            );
-          }else{
-            return SizedBox();
+                  );
+                },
+              );
+            case GetOccasionsErrorState():
+              aweSnackBar(
+                  msg: state.errorModel.error ?? '',
+                  context: context,
+                  type: MessageTypeConst.failure);
+            case OccasionsInitialState():
           }
+          return Container();
         },
       ),
     );
