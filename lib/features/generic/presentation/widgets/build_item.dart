525bac4cc4b2960ccea382c2c1cd964d60894d8c
Haidy <136759109+haidyhesham95@users.noreply.github.com>
2024-11-25 20:24:23 +0200

    refactor: use generic_build_item in build_item

--- lib/features/generic/presentation/widgets/build_item.dart
+++ lib/features/generic/presentation/widgets/build_item.dart
@@ -3,11 +3,8 @@ import 'package:flowery_e_commerce/features/best_seller/presentation/cubit/most_
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import '../../../../core/styles/colors/my_colors.dart';
-import '../../../../core/styles/fonts/my_fonts.dart';
-import '../../../../core/utils/widgets/buttons/row_button.dart';
-import '../../../../core/utils/widgets/spacing.dart';
-import 'cached_network_widget.dart';
+
+import '../generic_item_by_product/widget/generic_build_item.dart';
 
 
 class BuildItem extends StatelessWidget {
@@ -29,64 +26,12 @@ class BuildItem extends StatelessWidget {
             shrinkWrap: true,
             itemCount: state.mostSeller.length,
             itemBuilder: (BuildContext context, int index) {
-              return Container(
-                padding: const EdgeInsets.all(8.0),
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.circular(8.0),
-                  border: Border.all(color: MyColors.white70),
-                ),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    SizedBox(
-                      height: 140.h,
-                      child: CachedNetworkWidget(
-                        imageUrl: state.mostSeller[index].imageCover,
-                      ),
-                    ),
-                    verticalSpacing(8.h),
-                    Align(
-                      alignment: Alignment.center,
-                      widthFactor: 1.7,
-                      child: Text(
-                        state.mostSeller[index].title,
-                        style: MyFonts.styleRegular400_14.copyWith(
-                            color: MyColors.black),
-                      ),
-                    ),
-                    verticalSpacing(6.h),
-                    RichText(
-                      text: TextSpan(
-                        text: 'EGP ${state.mostSeller[index].price}',
-                        style: MyFonts.styleMedium500_14.copyWith(
-                            color: MyColors.black),
-                        children: [
-                          WidgetSpan(child: horizontalSpacing(5.w)),
-                          TextSpan(
-                            text: '${state.mostSeller[index].priceAfterDiscount}',
-                            style: MyFonts.styleRegular400_12.copyWith(
-                              color: MyColors.gray,
-                              decoration: TextDecoration.lineThrough,
-                            ),
-                          ),
-                          WidgetSpan(child: horizontalSpacing(5.w)),
-                          TextSpan(
-                            text: '20%',
-                            style: MyFonts.styleRegular400_12.copyWith(
-                                color: MyColors.green),
-                          ),
-                        ],
-                      ),
-                    ),
-                    verticalSpacing(8.h),
-                    Align(
-                        alignment: Alignment.bottomCenter,
-                        child: SizedBox(
-                            width: 150.w,
-                            child: RowButton(onTap: () {}))),
-                  ],
-                ),
+              return GenericBuildItem(
+                imageCover: state.mostSeller[index].imageCover,
+                title: state.mostSeller[index].title,
+                price: state.mostSeller[index].price.toString(),
+                priceAfterDiscount: state.mostSeller[index].priceAfterDiscount.toString(),
+
               );
             },);
         }else{
@@ -96,3 +41,4 @@ class BuildItem extends StatelessWidget {
     );
   }
 }
+
