fe1b4ec2cc975137e295018c50417b2c848f743f
Yasmeen Gad <gadyasmeen335@gmail.com>
2024-11-26 04:47:06 +0200

    feat: impl edit profile data source

--- lib/features/profile/data/data_sources/impl/profile_online_data_source_impl.dart
+++ lib/features/profile/data/data_sources/impl/profile_online_data_source_impl.dart
@@ -2,6 +2,7 @@ import 'package:flowery_e_commerce/core/networking/api/api_manager.dart';
 import 'package:flowery_e_commerce/core/networking/api_execute.dart';
 import 'package:flowery_e_commerce/core/networking/common/api_result.dart';
 import 'package:flowery_e_commerce/features/profile/data/mappers/profile_mappers.dart';
+import 'package:flowery_e_commerce/features/profile/domain/entities/response/edit_profile_response_entity.dart';
 
 import 'package:flowery_e_commerce/features/profile/domain/entities/response/get_logged_user_data_response_entity.dart';
 import 'package:injectable/injectable.dart';
@@ -21,6 +22,16 @@ class ProfileOnlineDataSourceImpl implements ProfileOnlineDataSource {
     return executeApi(() async {
       var response = await _apiManager.getLoggedUserData("Bearer $token");
       return ProfileMapper.toEntity(response);
+    });
+  }
+
+  @override
+  Future<DataResult<EditProfileResponseEntity>> editProfile(
+      String token, Map<String, dynamic> profileData) {
+    return executeApi(() async {
+      var response =
+          await _apiManager.editProfile("Bearer $token", profileData);
+      return ProfileMapper.editProfileToEntity(response);
     });
   }
 }
--- lib/features/profile/data/mappers/profile_mappers.dart
+++ lib/features/profile/data/mappers/profile_mappers.dart
@@ -1,3 +1,5 @@
+import 'package:flowery_e_commerce/features/profile/data/models/response/edit_profile_response_dto.dart';
+import 'package:flowery_e_commerce/features/profile/domain/entities/response/edit_profile_response_entity.dart';
 import 'package:flowery_e_commerce/features/profile/domain/entities/response/get_logged_user_data_response_entity.dart';
 
 import '../models/response/get_logged_user_data_response_dto.dart';
@@ -13,6 +15,26 @@ class ProfileMapper {
   static GetLoggedUserDataResponseUserEntity toUserEntity(
       GetLoggedUserDataResponseDtoUser dto) {
     return GetLoggedUserDataResponseUserEntity(
+        id: dto.id,
+        email: dto.email,
+        firstName: dto.firstName,
+        lastName: dto.lastName,
+        phone: dto.phone,
+        photo: dto.photo,
+        role: dto.role,
+        gender: dto.gender);
+  }
+
+  static EditProfileResponseEntity editProfileToEntity(
+      EditProfileResponseDto dto) {
+    return EditProfileResponseEntity(
+      user: editProfileToUserEntity(dto.user!),
+    );
+  }
+
+  static EditProfileResponseUserEntity editProfileToUserEntity(
+      EditProfileResponseDtoUser dto) {
+    return EditProfileResponseUserEntity(
         id: dto.id,
         email: dto.email,
         firstName: dto.firstName,
