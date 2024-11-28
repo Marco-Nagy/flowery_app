1147e3c0368227c8148a7a70c920609b462915db
Haidy <136759109+haidyhesham95@users.noreply.github.com>
2024-11-28 06:46:27 +0200

    fix: resolve warnings from linting

--- lib/core/networking/error/error.dart
+++
@@ -1,8 +0,0 @@
-// abstract class AppException implements Exception{
-//   final String message;
-//   const AppException ([this.message='Something went wrong!']);
-// }
-//
-// class RemoteException extends AppException {
-//   const RemoteException(super.message);
-// }
--- lib/core/networking/error/failure.dart
+++
@@ -1,9 +0,0 @@
-// class Failure {
-//   final String message;
-//
-//   const Failure(this.message);
-// }
-//
-// class RemoteFailure extends Failure {
-//   const RemoteFailure(super.message);
-// }
