2b880b74468524ca148a78bab8fbba53f29b04de
Yasmeen Gad <gadyasmeen335@gmail.com>
2024-11-29 22:59:52 +0200

    feat: create use case and repo interface

--- lib/features/profile/domain/repositories/profile_repo.dart
+++ lib/features/profile/domain/repositories/profile_repo.dart
@@ -1,6 +1,9 @@
+import 'dart:io';
+
 import '../../../../core/networking/common/api_result.dart';
 import '../entities/response/edit_profile_response_entity.dart';
 import '../entities/response/get_logged_user_data_response_entity.dart';
+import '../entities/response/upload_photo_response_entity.dart';
 
 abstract class ProfileRepo {
   Future<DataResult<GetLoggedUserDataResponseEntity>> getProfileData(
@@ -8,4 +11,7 @@ abstract class ProfileRepo {
 
   Future<DataResult<EditProfileResponseEntity>> editProfile(
       String token, Map<String, dynamic> profileData);
+
+  Future<DataResult<UploadPhotoResponseEntity>> uploadPhoto(
+      String token, File photo);
 }
--- lib/features/profile/domain/use_cases/profile_use_case.dart
+++ lib/features/profile/domain/use_cases/profile_use_case.dart
@@ -1,9 +1,12 @@
+import 'dart:io';
+
 import 'package:flowery_e_commerce/features/profile/domain/repositories/profile_repo.dart';
 import 'package:injectable/injectable.dart';
 
 import '../../../../core/networking/common/api_result.dart';
 import '../entities/response/edit_profile_response_entity.dart';
 import '../entities/response/get_logged_user_data_response_entity.dart';
+import '../entities/response/upload_photo_response_entity.dart';
 
 @injectable
 class ProfileUseCase {
@@ -19,4 +22,8 @@ class ProfileUseCase {
   Future<DataResult<EditProfileResponseEntity>> editProfile(
           String token, Map<String, dynamic> profileData) =>
       _repository.editProfile(token, profileData);
+
+  Future<DataResult<UploadPhotoResponseEntity>> uploadPhoto(
+          String token, File photo) =>
+      _repository.uploadPhoto(token, photo);
 }
