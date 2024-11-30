e224c86bf4249ac25e2e0adb30c2321f58225021
Marco Nagy <marconbishay@gmail.com>
2024-11-30 03:03:21 +0200

    Reapply "feat : handled Add to cart Button view on categories"
    This reverts commit ff6146ed4bd2b4fffe0b8734821bad4d1b20afa1.

--- lib/features/best_seller/presentation/screens/most_selling_screen.dart
+++ lib/features/best_seller/presentation/screens/most_selling_screen.dart
@@ -1,20 +1,14 @@
 import 'package:add_to_cart_animation/add_to_cart_animation.dart';
-import 'package:add_to_cart_animation/add_to_cart_icon.dart';
 import 'package:flowery_e_commerce/core/styles/colors/my_colors.dart';
 import 'package:flowery_e_commerce/core/styles/fonts/my_fonts.dart';
 import 'package:flowery_e_commerce/core/utils/widgets/custom_appbar.dart';
 import 'package:flowery_e_commerce/di/di.dart';
-import 'package:flowery_e_commerce/features/best_seller/domain/entities/most_selling_entity.dart';
-import 'package:flowery_e_commerce/features/best_seller/presentation/cubit/most_seller_states.dart';
 import 'package:flowery_e_commerce/features/best_seller/presentation/cubit/most_selling_cubit.dart';
 import 'package:flowery_e_commerce/features/cart/presentation/viewModel/cart_view_model_cubit.dart';
-import 'package:flowery_e_commerce/features/categories/presentation/categories/viewModel/categories_view_model_cubit.dart';
-import 'package:flowery_e_commerce/features/generic/presentation/generic_item_by_product/viewModel/generic_item_view_model_cubit.dart';
-import 'package:flowery_e_commerce/features/generic/presentation/generic_item_by_product/widget/add_to_cart_animation_widget.dart';
+import 'package:flowery_e_commerce/features/cart/presentation/widgets/cart_icon_badge.dart';
 import 'package:flowery_e_commerce/features/generic/presentation/widgets/build_item.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 import '../../../../core/utils/widgets/spacing.dart';
 
@@ -31,20 +25,21 @@ class _MostSellingScreenState extends State<MostSellingScreen> {
 
    late Function(GlobalKey) addToCartAnimation;
   final cartKey = GlobalKey<CartIconKey>();
-  CartViewModelCubit   viewModelCubit= getIt.get<CartViewModelCubit>();
+  CartViewModelCubit cartViewModelCubit = getIt.get<CartViewModelCubit>();
 
-   void listClick(GlobalKey widgetKey) async {
-     await addToCartAnimation(widgetKey);
-     await viewModelCubit.addToCart();
-    await viewModelCubit.cartKey.currentState!.runCartAnimation(viewModelCubit.cartQuantityItems.toString());
-   }
+  void listClick(GlobalKey widgetKey) async {
+    await addToCartAnimation(widgetKey);
+    await cartViewModelCubit.addToCart();
+    await cartViewModelCubit.cartKey.currentState!
+        .runCartAnimation(cartViewModelCubit.cartQuantityItems.toString());
+  }
+
   @override
   void initState() {
     super.initState();
-    // Initialize the cart count to 20
+    // Initialize the cart count to latest count
     WidgetsBinding.instance.addPostFrameCallback((_) {
-      // viewModelCubit.cartQuantityItems = 20;
-      viewModelCubit.updateCartCount();
+      cartViewModelCubit.updateCartCount();
     });
   }
 
@@ -53,7 +48,7 @@ class _MostSellingScreenState extends State<MostSellingScreen> {
     return BlocProvider(
       create: (context) => mostSellerCubit,
       child: AddToCartAnimation(
-        cartKey: viewModelCubit.cartKey,
+        cartKey: cartViewModelCubit.cartKey,
         height: 30,
         width: 30,
         opacity: 0.85,
@@ -63,9 +58,6 @@ class _MostSellingScreenState extends State<MostSellingScreen> {
         jumpAnimation: const JumpAnimationOptions(),
         createAddToCartAnimation: (runAddToCartAnimation) {
           addToCartAnimation = runAddToCartAnimation;
-
-
-          // return (GlobalKey) => addToCartAnimation(GlobalKey(cartKey));
         },
         child: Scaffold(
           backgroundColor: Colors.white,
@@ -75,14 +67,7 @@ class _MostSellingScreenState extends State<MostSellingScreen> {
             showArrow: true,
               actions: [
                 const SizedBox(width: 16),
-                AddToCartIcon(
-                  key: viewModelCubit.cartKey,
-                  icon: const Icon(Icons.shopping_cart),
-                  badgeOptions: const BadgeOptions(
-                    active: true,
-                    backgroundColor: Colors.red,
-                  ),
-                ),
+                CartIconBadge(cartKey: cartViewModelCubit.cartKey, ),
                 const SizedBox(
                   width: 16,
                 )
