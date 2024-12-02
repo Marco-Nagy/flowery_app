85460043b23e8c2423378add7353f9fbb7d1698f
Marco Nagy <marco.vetanoia@gmail.com>
2024-12-02 15:56:06 +0200

    fix : linting files

--- lib/features/cart/presentation/screens/cart_view.dart
+++ lib/features/cart/presentation/screens/cart_view.dart
@@ -1,18 +1,10 @@
-import 'package:flowery_e_commerce/core/styles/colors/my_colors.dart';
-import 'package:flowery_e_commerce/core/styles/fonts/my_fonts.dart';
 import 'package:flowery_e_commerce/core/utils/widgets/base/app_loader.dart';
-import 'package:flowery_e_commerce/core/utils/widgets/base/base_view.dart';
-import 'package:flowery_e_commerce/core/utils/widgets/base/custom_app_bar.dart';
-import 'package:flowery_e_commerce/core/utils/widgets/custom_appbar.dart';
 import 'package:flowery_e_commerce/di/di.dart';
 import 'package:flowery_e_commerce/features/cart/domain/entities/cart_entity.dart';
-import 'package:flowery_e_commerce/features/cart/presentation/viewModel/cart_base_action.dart';
 import 'package:flowery_e_commerce/features/cart/presentation/viewModel/cart_view_model_cubit.dart';
 import 'package:flowery_e_commerce/features/cart/presentation/widgets/cart_view_body.dart';
-import 'package:flowery_e_commerce/generated/assets.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class CartView extends StatefulWidget {
   const CartView({super.key});
