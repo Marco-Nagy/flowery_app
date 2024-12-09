16937127a6a210fd35fddfd61d1338902f11764e
Marco Nagy <marconbishay@gmail.com>
2024-12-09 15:15:25 +0200

    feat : update Checkout Screen UI

--- lib/features/cart/presentation/widgets/check_out/payment_option.dart
+++ lib/features/cart/presentation/widgets/check_out/payment_option.dart
@@ -2,22 +2,50 @@ import 'package:flutter/material.dart';
 
 class PaymentOption extends StatelessWidget {
   final String title;
-  final bool isSelected;
+  final String selectedValue;
+  final Function(String) onSelected;
 
-  const PaymentOption({super.key, required this.title, this.isSelected = false});
+  const PaymentOption({
+    super.key,
+    required this.title,
+    required this.selectedValue,
+    required this.onSelected,
+  });
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: [
-        Radio(
-          value: true,
-          groupValue: isSelected,
-          onChanged: (value) {},
-          activeColor: Colors.pink,
+    final bool isSelected = selectedValue == title;
+
+    return GestureDetector(
+      onTap: () => onSelected(title),
+      child: Container(
+        padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
+        decoration: BoxDecoration(
+          color: Colors.white,
+          border: Border.all(
+            color: Colors.grey.shade300,
+            width: 1,
+          ),
+          borderRadius: BorderRadius.circular(12),
         ),
-        Text(title),
-      ],
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text(
+              title,
+              style: const TextStyle(fontSize: 16, color: Colors.black87),
+            ),
+            Icon(
+              isSelected
+                  ? Icons.radio_button_checked
+                  : Icons.radio_button_off,
+              color: Colors.pink,
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
+
+
