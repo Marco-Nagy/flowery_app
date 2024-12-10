de185bb66b7587d78913bd8bbeb6858352101f0e
Haidy <136759109+haidyhesham95@users.noreply.github.com>
2024-12-11 00:39:07 +0200

    feat: use FadeInLeft in home

--- lib/features/home_screen/presentation/widgets/custom_best_seller_list.dart
+++ lib/features/home_screen/presentation/widgets/custom_best_seller_list.dart
@@ -1,3 +1,4 @@
+import 'package:animate_do/animate_do.dart';
 import 'package:flowery_e_commerce/core/styles/colors/my_colors.dart';
 import 'package:flowery_e_commerce/core/styles/fonts/my_fonts.dart';
 import 'package:flowery_e_commerce/core/utils/widgets/base/app_loader.dart';
@@ -25,46 +26,54 @@ class CustomBestSellerListState extends State<CustomBestSellerList> {
       height: 270.h,
       child: BlocBuilder<BestSellerCubit, BestSellerStates>(
         builder: (context, state) {
-         switch (state) {
+          switch (state) {
             case GetBestSellerSuccessState():
               return ListView.builder(
                 itemCount: state.bestSeller.length,
                 scrollDirection: Axis.horizontal,
                 itemBuilder: (context, index) {
-                  return GestureDetector(
-                 onTap: () => Navigator.pushNamed(context, AppRoutes.productsDetailsView, arguments: state.bestSeller[index]),
-                 child:Container(
-                    margin: EdgeInsets.all(3.sp),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        SizedBox(
-                          width: 170.w,
-                          height: 180.h,
-                          child: CachedNetworkWidget(
-                            imageUrl: state.bestSeller[index].imgCover??'',
-                          fit: BoxFit.cover,),
+                  return FadeInLeft(
+                    curve: Curves.linear,
+                    duration: Duration(milliseconds: 100 * (index + 5)),
+                    child: GestureDetector(
+                      onTap: () => Navigator.pushNamed(
+                          context, AppRoutes.productsDetailsView,
+                          arguments: state.bestSeller[index]),
+                      child: Container(
+                        margin: EdgeInsets.all(3.sp),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            SizedBox(
+                              width: 170.w,
+                              height: 180.h,
+                              child: CachedNetworkWidget(
+                                imageUrl:
+                                    state.bestSeller[index].imgCover ?? '',
+                                fit: BoxFit.cover,
+                              ),
+                            ),
+                            SizedBox(
+                              height: 8.h,
+                            ),
+                            SizedBox(
+                              width: 170.w,
+                              child: Text(
+                                state.bestSeller[index].title ?? '',
+                                style: MyFonts.styleRegular400_14.copyWith(
+                                    color: MyColors.blackBase,
+                                    overflow: TextOverflow.visible),
+                                maxLines: 2,
+                              ),
+                            ),
+                            SizedBox(
+                              height: 3.h,
+                            ),
+                            Text('${state.bestSeller[index].price}',
+                                style: MyFonts.styleMedium500_14
+                                    .copyWith(color: MyColors.blackBase)),
+                          ],
                         ),
-                        SizedBox(
-                          height: 8.h,
-                        ),
-                        SizedBox(
-                          width: 170.w,
-                          child: Text(
-                            state.bestSeller[index].title??'',
-                            style: MyFonts.styleRegular400_14.copyWith(
-                                color: MyColors.blackBase,
-                                overflow: TextOverflow.visible),
-                              maxLines: 2,
-                          ),
-                        ),
-                        SizedBox(
-                          height: 3.h,
-                        ),
-                        Text('${state.bestSeller[index].price}',
-                            style: MyFonts.styleMedium500_14
-                                .copyWith(color: MyColors.blackBase)),
-                     ],
                       ),
                     ),
                   );
@@ -81,7 +90,7 @@ class CustomBestSellerListState extends State<CustomBestSellerList> {
             default:
               null;
           }
-         return Container();
+          return Container();
         },
       ),
     );
