cf09a16a934c32fc4b5d6de4d6f55d3434e9f8d9
Yasmeen Gad <gadyasmeen335@gmail.com>
2024-12-13 00:59:57 +0200

    fix: fix linting

--- lib/flowery_ecommerce.dart
+++ lib/flowery_ecommerce.dart
@@ -24,7 +24,7 @@ class FloweryEcommerce extends StatelessWidget {
             minTextAdapt: true,
             splitScreenMode: true,
             builder: (_, child) => MaterialApp(
-              initialRoute: AppRoutes.addressScreen,
+              initialRoute: _getInitialRoute(),
               debugShowCheckedModeBanner: false,
               builder: (context, child) =>
                   Scaffold(
