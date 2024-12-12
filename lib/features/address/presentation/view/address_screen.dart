8d143407204a8ef25d12483f672b4d771ec5c02c
Yasmeen Gad <gadyasmeen335@gmail.com>
2024-12-13 00:47:31 +0200

    chore: integrate country state city package with text field

--- lib/features/address/presentation/view/address_screen.dart
+++ lib/features/address/presentation/view/address_screen.dart
@@ -1,9 +1,9 @@
+import 'package:country_state_city/models/city.dart';
+import 'package:country_state_city/models/country.dart';
+import 'package:country_state_city/utils/utils.dart';
 import 'package:flowery_e_commerce/core/styles/colors/my_colors.dart';
 import 'package:flowery_e_commerce/core/utils/widgets/base/snack_bar.dart';
 import 'package:flowery_e_commerce/core/utils/widgets/buttons/carved_button.dart';
-import 'package:flowery_e_commerce/core/utils/widgets/custom_appbar.dart';
-import 'package:flowery_e_commerce/features/address/presentation/widgets/build_city_area_widget.dart';
-import 'package:flowery_e_commerce/features/address/presentation/widgets/build_text_field_widget.dart';
 import 'package:flowery_e_commerce/features/address_details/domain/entities/request/add_address_request_entity.dart';
 import 'package:flowery_e_commerce/features/address_details/presentation/viewModel/add_address_action.dart';
 import 'package:flowery_e_commerce/features/address_details/presentation/viewModel/add_address_view_model_cubit.dart';
@@ -11,6 +11,9 @@ import 'package:flowery_e_commerce/generated/assets.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import '../../../../core/utils/widgets/custom_appbar.dart';
+import '../widgets/build_dropdown_field_widget.dart';
+import '../widgets/build_text_field_widget.dart';
 
 class AddressScreen extends StatefulWidget {
   const AddressScreen({super.key});
@@ -23,8 +26,12 @@ class _AddressScreenState extends State<AddressScreen> {
   late final TextEditingController addressController;
   late final TextEditingController phoneController;
   late final TextEditingController recipientController;
-  late final TextEditingController cityController;
-  late final TextEditingController areaController;
+
+  String? selectedCountry;
+  String? selectedCity;
+
+  List<Country> countryList = [];
+  List<City> cityList = [];
 
   bool isButtonEnabled = false;
 
@@ -34,8 +41,7 @@ class _AddressScreenState extends State<AddressScreen> {
     addressController = TextEditingController()..addListener(_checkFields);
     phoneController = TextEditingController()..addListener(_checkFields);
     recipientController = TextEditingController()..addListener(_checkFields);
-    cityController = TextEditingController()..addListener(_checkFields);
-    areaController = TextEditingController()..addListener(_checkFields);
+    fetchCountries();
   }
 
   @override
@@ -43,8 +49,6 @@ class _AddressScreenState extends State<AddressScreen> {
     addressController.dispose();
     phoneController.dispose();
     recipientController.dispose();
-    cityController.dispose();
-    areaController.dispose();
     super.dispose();
   }
 
@@ -53,8 +57,23 @@ class _AddressScreenState extends State<AddressScreen> {
       isButtonEnabled = addressController.text.isNotEmpty &&
           phoneController.text.isNotEmpty &&
           recipientController.text.isNotEmpty &&
-          cityController.text.isNotEmpty &&
-          areaController.text.isNotEmpty;
+          selectedCountry != null &&
+          selectedCity != null;
+    });
+  }
+
+  Future<void> fetchCountries() async {
+    final countries = await getAllCountries();
+    setState(() {
+      countryList = countries;
+    });
+  }
+
+  Future<void> fetchCities(String countryCode) async {
+    final cities = await getCountryCities(countryCode);
+    debugPrint("Fetched Cities: $cities");
+    setState(() {
+      cityList = cities;
     });
   }
 
@@ -72,34 +91,70 @@ class _AddressScreenState extends State<AddressScreen> {
             child: Column(
               children: [
                 Container(
-                    margin: EdgeInsets.all(5.sp),
-                    width: 390.w,
-                    height: 200.h,
-                    child: Image.asset(Assets.imagesRectangle)),
+                  margin: EdgeInsets.only(bottom: 10.h),
+                  width: 390.w,
+                  height: 200.h,
+                  child: Image.asset(Assets.imagesRectangle, fit: BoxFit.cover),
+                ),
                 BuildTextFieldWidget(
-                    hint: "Enter the address",
-                    label: "Address",
-                    controller: addressController),
+                  hint: "Enter the address",
+                  label: "Address",
+                  controller: addressController,
+                ),
+                SizedBox(height: 10.h),
                 BuildTextFieldWidget(
-                    hint: "Enter the phone number",
-                    label: "Phone number",
-                    controller: phoneController),
+                  hint: "Enter the phone number",
+                  label: "Phone number",
+                  controller: phoneController,
+                ),
+                SizedBox(height: 10.h),
                 BuildTextFieldWidget(
-                    hint: "Enter the recipient name",
-                    label: "Recipient name",
-                    controller: recipientController),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  children: [
-                    BuildCityAreaWidget(
-                        label: "City",
-                        hint: "Cairo",
-                        controller: cityController),
-                    BuildCityAreaWidget(
-                        label: "Area",
-                        hint: "October",
-                        controller: areaController),
-                  ],
+                  hint: "Enter the recipient name",
+                  label: "Recipient name",
+                  controller: recipientController,
+                ),
+                SizedBox(height: 10.h),
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 12.r),
+                  child: Row(
+                    children: [
+                      Expanded(
+                        child: BuildDropdownFieldWidget(
+                          hint: "Select Country",
+                          label: "Country",
+                          items: countryList,
+                          selectedValue: selectedCountry,
+                          onChanged: (value) {
+                            setState(() {
+                              selectedCountry = value;
+                              cityList
+                                  .clear(); // Clear city list when country changes
+                              _checkFields();
+                              fetchCities(countryList
+                                      .firstWhere((item) => item.name == value)
+                                      .isoCode ??
+                                  "");
+                            });
+                          },
+                        ),
+                      ),
+                      SizedBox(width: 10.h),
+                      Expanded(
+                        child: BuildDropdownFieldWidget(
+                          hint: "Select City",
+                          label: "City",
+                          items: cityList,
+                          selectedValue: selectedCity,
+                          onChanged: (value) {
+                            setState(() {
+                              selectedCity = value;
+                              _checkFields();
+                            });
+                          },
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
                 SizedBox(height: 20.h),
                 SizedBox(
@@ -111,17 +166,17 @@ class _AddressScreenState extends State<AddressScreen> {
                         ? () {
                             context.read<AddAddressViewModelCubit>().doAction(
                                   AddAddressSubmitAction(
-                                      AddAddressRequestEntity(
-                                    street: addressController.text.trim(),
-                                    city: cityController.text.trim(),
-                                    phone: phoneController.text.trim(),
-                                  )),
+                                    AddAddressRequestEntity(
+                                      street: addressController.text.trim(),
+                                      city: selectedCity,
+                                      phone: phoneController.text.trim(),
+                                    ),
+                                  ),
                                 );
                           }
-                        : () {}, // Disabled if fields are not filled
-                    color: isButtonEnabled
-                        ? MyColors.baseColor
-                        : MyColors.gray30, // Change color conditionally
+                        : () {},
+                    color:
+                        isButtonEnabled ? MyColors.baseColor : MyColors.gray30,
                   ),
                 ),
               ],
@@ -132,26 +187,30 @@ class _AddressScreenState extends State<AddressScreen> {
           switch (state) {
             case AddAddressViewModelLoading():
               aweSnackBar(
-                  msg: 'loading...',
-                  context: context,
-                  type: MessageTypeConst.help,
-                  title: 'Loading');
+                msg: 'Loading...',
+                context: context,
+                type: MessageTypeConst.help,
+                title: 'Loading',
+              );
               break;
             case AddAddressViewModelSuccess():
               aweSnackBar(
-                  msg: 'success',
-                  context: context,
-                  type: MessageTypeConst.success,
-                  title: 'success');
+                msg: 'Address saved successfully!',
+                context: context,
+                type: MessageTypeConst.success,
+                title: 'Success',
+              );
               break;
             case AddAddressViewModelError():
               aweSnackBar(
-                  msg: state.message.error ?? "Something went wrong, try again",
-                  context: context,
-                  type: MessageTypeConst.failure,
-                  title: 'Failure');
+                msg: state.message.error ?? "Something went wrong, try again",
+                context: context,
+                type: MessageTypeConst.failure,
+                title: 'Failure',
+              );
               break;
             default:
+              break;
           }
         },
       ),
