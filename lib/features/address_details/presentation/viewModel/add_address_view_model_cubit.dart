9e1f68ea640991a133ca7f83beb9c640cb7afe7a
Yasmeen Gad <gadyasmeen335@gmail.com>
2024-12-13 18:01:18 +0200

    refactor: refactor address screen to satisfy clean architecture principles

--- lib/features/address_details/presentation/viewModel/add_address_view_model_cubit.dart
+++ lib/features/address_details/presentation/viewModel/add_address_view_model_cubit.dart
@@ -1,4 +1,7 @@
 import 'package:bloc/bloc.dart';
+import 'package:country_state_city/models/city.dart';
+import 'package:country_state_city/models/country.dart';
+import 'package:country_state_city/utils/utils.dart';
 import 'package:flowery_e_commerce/core/networking/common/api_result.dart';
 import 'package:flowery_e_commerce/core/networking/error/error_model.dart';
 import 'package:flowery_e_commerce/features/address_details/presentation/viewModel/add_address_action.dart';
@@ -15,7 +18,8 @@ part 'add_address_view_model_state.dart';
 @injectable
 class AddAddressViewModelCubit extends Cubit<AddAddressViewModelState> {
   final AddAddressUseCase _addAddressUseCase;
-
+  List<Country> countryList = [];
+  List<City> cityList = [];
   @factoryMethod
   AddAddressViewModelCubit(this._addAddressUseCase)
       : super(AddAddressViewModelInitial());
@@ -24,6 +28,12 @@ class AddAddressViewModelCubit extends Cubit<AddAddressViewModelState> {
     switch (action) {
       case AddAddressSubmitAction():
         _addAddress(action.request);
+        break;
+      case FetchCountriesAction():
+        _fetchCountries();
+        break;
+      case FetchCitiesAction():
+        _fetchCities(action.countryCode);
         break;
     }
   }
@@ -38,6 +48,28 @@ class AddAddressViewModelCubit extends Cubit<AddAddressViewModelState> {
       case Fail<AddAddressResponseEntity>():
         emit(AddAddressViewModelError(ErrorHandler.handle(result.exception!)));
         break;
+    }
+  }
+
+  Future<void> _fetchCountries() async {
+    try {
+      final countries =
+          await getAllCountries(); // Replace with your actual use case
+      countryList = countries;
+      emit(AddAddressViewModelFetchedCountries(countries));
+    } on Exception catch (e) {
+      emit(AddAddressViewModelError(ErrorHandler.handle(e)));
+    }
+  }
+
+  Future<void> _fetchCities(String countryCode) async {
+    try {
+      final cities =
+          await getCountryCities(countryCode); // Replace with actual use case
+      cityList = cities;
+      emit(AddAddressViewModelFetchedCities(cities));
+    } on Exception catch (e) {
+      emit(AddAddressViewModelError(ErrorHandler.handle(e)));
     }
   }
 }
