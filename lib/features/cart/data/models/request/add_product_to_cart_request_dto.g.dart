7fa0d24a44673a7a2a366a297e23b7594b960bb4
Yasmeen Gad <gadyasmeen335@gmail.com>
2024-12-13 17:32:01 +0200

    fix: fix linting

--- lib/features/cart/data/models/request/add_product_to_cart_request_dto.g.dart
+++ lib/features/cart/data/models/request/add_product_to_cart_request_dto.g.dart
@@ -6,13 +6,6 @@ part of 'add_product_to_cart_request_dto.dart';
 // JsonSerializableGenerator
 // **************************************************************************
 
-AddProductToCartRequestDto _$AddProductToCartRequestDtoFromJson(
-        Map<String, dynamic> json) =>
-    AddProductToCartRequestDto(
-      product: json['product'] as String?,
-      quantity: (json['quantity'] as num?)?.toInt(),
-    );
-
 Map<String, dynamic> _$AddProductToCartRequestDtoToJson(
         AddProductToCartRequestDto instance) =>
     <String, dynamic>{
