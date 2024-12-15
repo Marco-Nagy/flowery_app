131f60098f9b593d4fd3ac773ed58888c491a10d
Marco Nagy <marconbishay@gmail.com>
2024-12-15 09:44:41 +0200

    fix : merge conflicts

--- windows/flutter/generated_plugins.cmake
+++ windows/flutter/generated_plugins.cmake
@@ -5,8 +5,8 @@
 list(APPEND FLUTTER_PLUGIN_LIST
   connectivity_plus
   file_selector_windows
-  flutter_localization
   firebase_core
+  flutter_localization
 )
 
 list(APPEND FLUTTER_FFI_PLUGIN_LIST
