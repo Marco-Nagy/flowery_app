87dbac3b3b84520005ca3d2d6581537a012d1f1c
Yasmeen Gad <gadyasmeen335@gmail.com>
2024-12-16 03:25:45 +0200

    feat(notification_list): create section_notification_message

--- lib/features/notification_list/presentation/widgets/section_notification_message.dart
+++ lib/features/notification_list/presentation/widgets/section_notification_message.dart
@@ -1,4 +1,7 @@
+import 'package:auto_size_text/auto_size_text.dart';
+import 'package:flowery_e_commerce/core/utils/widgets/spacing.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 import '../../../../core/styles/colors/my_colors.dart';
 import '../../../../core/styles/fonts/my_fonts.dart';
@@ -10,16 +13,35 @@ class SectionNotificationMessage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: ListTile(
-        leading: Image.asset("${Assets.imagesNotification}"),
-        title: Text(
-          'New Offer',
-          style: MyFonts.styleMedium500_16.copyWith(color: MyColors.baseColor),
-        ),
-        subtitle: Text(
-          'Lorem ipsum dolor sit amet consectetur. Tristique et mauris sem congue in felis id nec. Amet sed morbi bibendum vestibulum.',
-          style: MyFonts.styleRegular400_12.copyWith(color: MyColors.gray),
-        ),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          ImageIcon(
+            const AssetImage(Assets.imagesNotificationList),
+            size: 20.sp,
+          ),
+          horizontalSpacing(12.w),
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                AutoSizeText(
+                  'New Offer',
+                  style: MyFonts.styleMedium500_16.copyWith(
+                    color: MyColors.baseColor,
+                  ),
+                ),
+                verticalSpacing(4.h),
+                AutoSizeText(
+                  'Lorem ipsum dolor sit amet consectetur. Tristique et mauris sem congue in felis id nec. Amet sed morbi bibendum vestibulum.',
+                  style: MyFonts.styleRegular400_12.copyWith(
+                    color: MyColors.gray,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
