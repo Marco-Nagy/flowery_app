c95b5ac5286d871e1dc2bdd4b329b2c9d3208527
Yasmeen Gad <gadyasmeen335@gmail.com>
2024-12-16 02:55:26 +0200

    feat(notification_list): create app bar and add route of view

---
+++ lib/features/notification_list/presentation/views/notification_list_view.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import '../../../../core/utils/widgets/base/base_view.dart';
+import '../../../../core/utils/widgets/base/custom_app_bar.dart';
+import '../../../../core/utils/widgets/spacing.dart';
+
+class NotificationView extends StatelessWidget {
+  const NotificationView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return BaseView(
+      child: CustomScrollView(
+        slivers: [
+          SliverToBoxAdapter(
+            child: Column(
+              children: [
+                CustomAppBar(
+                  appBarTxt: '${AppLocalizations.of(context)!.notification}',
+                  showArrow: true,
+                ),
+                verticalSpacing(32.h),
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
