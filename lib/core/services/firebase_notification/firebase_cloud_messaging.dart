5751b8d1a0bb3595df78a25fa1483abaa81269c3
Yasmeen Gad <gadyasmeen335@gmail.com>
2024-12-17 23:53:57 +0200

    feat(notification_list): update some changes

--- lib/core/services/firebase_notification/firebase_cloud_messaging.dart
+++ lib/core/services/firebase_notification/firebase_cloud_messaging.dart
@@ -1,10 +1,16 @@
 import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flowery_e_commerce/core/routes/app_routes.dart';
+import 'package:flowery_e_commerce/core/utils/extension/navigation.dart';
 import 'package:flutter/cupertino.dart';
+
+import '../../../di/di.dart';
 
 class FirebaseCloudMessaging {
   factory FirebaseCloudMessaging() => _instance;
   static final FirebaseCloudMessaging _instance =
       FirebaseCloudMessaging._internal();
+  static final GlobalKey<NavigatorState> navigatorKey =
+      getIt<GlobalKey<NavigatorState>>();
 
   FirebaseCloudMessaging._internal();
 
@@ -24,6 +30,7 @@ class FirebaseCloudMessaging {
   static Future<void> _firebaseMessagingBackgroundHandler(
       RemoteMessage message) async {
     // Handle background messages here
+    navigatorKey.currentState?.context.pushNamed(AppRoutes.notificationView);
     debugPrint("Handling a background message: ${message.notification}");
   }
 
