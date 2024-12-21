80ce3d74653e4523075bb5ee5eb3e2a9f3fbb165
Marco Nagy <marconbishay@gmail.com>
2024-12-21 21:08:15 +0200

    refactor: navigation to cart screen from cart icon

--- lib/core/routes/app_routes.dart
+++ lib/core/routes/app_routes.dart
@@ -10,8 +10,10 @@ import 'package:flowery_e_commerce/features/auth/presentation/signup/view_model/
 import 'package:flowery_e_commerce/features/best_seller/presentation/cubit/most_selling_cubit.dart';
 import 'package:flowery_e_commerce/features/best_seller/presentation/screens/most_selling_screen.dart';
 import 'package:flowery_e_commerce/features/cart/domain/entities/cart_entity.dart';
+import 'package:flowery_e_commerce/features/cart/presentation/view/cart_view.dart';
 import 'package:flowery_e_commerce/features/cart/presentation/viewModel/cart_base_action.dart';
 import 'package:flowery_e_commerce/features/cart/presentation/viewModel/cart_view_model_cubit.dart';
+import 'package:flowery_e_commerce/features/cart/presentation/widgets/cart_view_body.dart';
 import 'package:flowery_e_commerce/features/checkout/presentation/view/checkout_screen.dart';
 import 'package:flowery_e_commerce/features/checkout/presentation/viewModel/checkout_view_model_cubit.dart';
 import 'package:flowery_e_commerce/features/generic/presentation/generic_item_by_product/viewModel/generic_item_action.dart';
@@ -55,6 +57,7 @@ class AppRoutes {
   static const String profileView = "profileView";
   static const String resetPasswordProfileView = 'resetPasswordProfileView';
   static const String checkoutScreen = 'checkoutScreen';
+  static const String cartScreen = 'cartScreen';
 
   static Route<void> onGenerateRoute(RouteSettings settings) {
     final args = settings.arguments;
@@ -127,7 +130,11 @@ class AppRoutes {
       case AppRoutes.profileMainScreen:
         return BaseRoute(page: const ProfileMainScreen());
       case AppRoutes.productsDetailsView:
-        return BaseRoute(page: ProductDetailsView(product: args  as dynamic,));
+        return BaseRoute(page:      BlocProvider(
+    create: (context) => getIt.get<CartViewModelCubit>()
+    ..doAction(
+    GetUserCartDataAction(),
+    ),child: ProductDetailsView(product: args  as dynamic,)));
 
       case AppRoutes.profileView:
         return BaseRoute(page: const ProfileView());
@@ -156,7 +163,15 @@ class AppRoutes {
       case AppRoutes.termsAndConditionsPage:
         return BaseRoute(page: const TermsAndConditionsPage());
         case AppRoutes.orderView:
-        return BaseRoute(page: const OrderView());
+        return BaseRoute(page: const OrderView()); 
+        case AppRoutes.cartScreen:
+        return BaseRoute(page:       BlocProvider(
+    create: (context) => getIt.get<CartViewModelCubit>()
+    ..doAction(
+    GetUserCartDataAction(),
+    ),
+  child: const CartView( backButtonVisible: true),
+));
       default:
         return BaseRoute(page: const PageUnderBuildScreen());
     }
--- lib/features/product/presentation/view/product_details_view.dart
+++ lib/features/product/presentation/view/product_details_view.dart
@@ -1,23 +1,95 @@
+import 'package:add_to_cart_animation/add_to_cart_animation.dart';
+import 'package:flowery_e_commerce/core/utils/widgets/base/base_view.dart';
+import 'package:flowery_e_commerce/features/cart/presentation/viewModel/cart_base_action.dart';
+import 'package:flowery_e_commerce/features/cart/presentation/viewModel/cart_view_model_cubit.dart';
+import 'package:flowery_e_commerce/features/cart/presentation/widgets/cart_icon_badge.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 import '../widgets/arrow_back_widget.dart';
 import '../widgets/product_details_widget.dart';
 
-class ProductDetailsView extends StatelessWidget {
+class ProductDetailsView extends StatefulWidget {
   const ProductDetailsView({super.key, required this.product});
 
   final dynamic product;
 
   @override
+  State<ProductDetailsView> createState() => _ProductDetailsViewState();
+}
+
+class _ProductDetailsViewState extends State<ProductDetailsView> {
+  late Function(GlobalKey) addToCartAnimation;
+  final GlobalKey widgetKey = GlobalKey();
+
+  void listClick(GlobalKey widgetKey) async {
+    CartViewModelCubit cartViewModelCubit = context.read<CartViewModelCubit>();
+
+    await cartViewModelCubit.doAction(GetUserCartDataAction());
+    addToCartAnimation(widgetKey);
+
+    cartViewModelCubit.cartKey.currentState!
+        .runCartAnimation(cartViewModelCubit.cartQuantityItems.toString());
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    CartViewModelCubit cartViewModelCubit = context.read<CartViewModelCubit>();
+
+    // Initialize the cart count to 20
+    WidgetsBinding.instance.addPostFrameCallback((_) async {
+      cartViewModelCubit.doAction(GetUserCartDataAction());
+      // Check if cartQuantityItems is initialized
+        cartViewModelCubit.cartKey.currentState!
+            .runCartAnimation(cartViewModelCubit.cartQuantityItems.toString());
+
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white,
-      body: SingleChildScrollView(
-        child: Stack(children: [
-          ProductDetailsWidget(product: product),
-          arrowBackWidget(context),
-        ]),
+    CartViewModelCubit cartViewModelCubit = context.read<CartViewModelCubit>();
+
+    return AddToCartAnimation(
+      cartKey: cartViewModelCubit.cartKey,
+      height: 30,
+      width: 30,
+      opacity: 0.85,
+      dragAnimation: const DragToCartAnimationOptions(
+        rotation: true,
+      ),
+      jumpAnimation: const JumpAnimationOptions(),
+      createAddToCartAnimation: (runAddToCartAnimation) {
+        addToCartAnimation = runAddToCartAnimation;
+      },
+      child: Scaffold(
+        backgroundColor: Colors.white,
+        body: Stack(
+          children: [
+            SingleChildScrollView(
+              child: ProductDetailsWidget(
+                product: widget.product,
+                onClick: (widgetKey) => listClick(widgetKey),
+              ),
+            ),
+            // Combine Positioned widgets into the same Stack
+            Positioned(
+              top: 30,
+              left: 10,
+              child: arrowBackWidget(context),
+            ),
+            Positioned(
+              top: 30,
+              right: 10,
+              child: CartIconBadge(
+                cartKey: cartViewModelCubit.cartKey,
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
+
 }
