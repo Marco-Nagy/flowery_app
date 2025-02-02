44e35267494f0e9b7af2674a24c56b4a79c69819
Marco Nagy <marconbishay@gmail.com>
2025-02-03 01:17:00 +0200

    feat : Added Track Order Screen

--- lib/core/services/firebase_helper/fire_store_helper.dart
+++ lib/core/services/firebase_helper/fire_store_helper.dart
@@ -1,4 +1,4 @@
-  import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_database/firebase_database.dart';
 
 import 'package:injectable/injectable.dart';
