abbe0661482cc5143a24d7d71fa7f9fcca0664ec
Haidy <136759109+haidyhesham95@users.noreply.github.com>
2025-02-03 17:16:23 +0200

    feat : edit my orders page

--- lib/features/orders/presentation/widget/order_list_widget.dart
+++ lib/features/orders/presentation/widget/order_list_widget.dart
@@ -30,73 +30,79 @@ class ListOrderWidget extends StatelessWidget {
             if (successState.orders == null || successState.orders!.isEmpty) {
               return Center(
                 child: Text(
-                 context.translate(LangKeys.noOrdersAvailable),
+                  context.translate(LangKeys.noOrdersAvailable),
                   style: MyFonts.styleMedium500_14,
                 ),
               );
             }
-            return ListView.separated(
-                padding: EdgeInsets.symmetric(horizontal: 25.w),
-                separatorBuilder: (context, index) => SizedBox(height: 16.h),
-                itemCount: successState.orders!.length,
-                itemBuilder: (context, index) {
-                  return Container(
-                    height: 125.h,
-                    padding: const EdgeInsets.all(8.0),
-                    decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.circular(8.0),
-                      border: Border.all(color: MyColors.white70),
-                    ),
-                    child: Row(
-                      children: [
-                        Expanded(
+            return ListView.builder(
+              padding: EdgeInsets.symmetric(horizontal: 25.w),
+              itemCount: successState.orders!.length,
+              itemBuilder: (context, index) {
+                final order = successState.orders![index];
+                return Column(
+                  children: order.orderItems!.map((orderItem) {
+                    return Container(
+                      margin: EdgeInsets.symmetric(vertical: 12.w),
+                      height: 125.h,
+                      padding: const EdgeInsets.all(8.0),
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(8.0),
+                        border: Border.all(color: MyColors.white70),
+                      ),
+                      child: Row(
+                        children: [
+                          Expanded(
                             child: CachedNetworkWidget(
-                          imageUrl: successState.orders![index]
-                                  .orderItems![index].product!.imgCover ??
-                              Assets.imagesFlower,
-                          fit: BoxFit.cover,
-                          height: 109.h,
-                          //  width: 127.w,
-                        )),
-                        horizontalSpacing(16.w),
-                        Expanded(
-                          flex: 2,
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            mainAxisAlignment: MainAxisAlignment.spaceAround,
-                            children: [
-                              Text(
-                                successState.orders![index].orderItems![index]
-                                    .product!.title!,
-                                style: MyFonts.styleRegular400_12,
-                              ),
-                              Text(
-                                  '${context.translate(LangKeys.egp)} ${successState.orders![index].orderItems![index].product!.price.toString()}',
-                                  style: MyFonts.styleMedium500_12),
-                              Text(
-                                  '${context.translate(LangKeys.orderNumber)} ${successState.orders![index].orderNumber}',
-                                  style: MyFonts.styleRegular400_12
-                                      .copyWith(color: MyColors.grey)),
-                              verticalSpacing(8.h),
-                              Align(
-                                alignment: Alignment.centerLeft,
-                                child: CurvedButton(
-                                  height: 30.h,
-                                  style: MyFonts.styleMedium500_13
-                                      .copyWith(color: MyColors.white),
-                                  color: MyColors.baseColor,
-                                  title: textButton,
-                                  onTap: () {},
-                                ),
-                              )
-                            ],
+                              imageUrl: orderItem.product!.imgCover ??
+                                  Assets.imagesFlower,
+                              fit: BoxFit.cover,
+                              height: 109.h,
+                            ),
                           ),
-                        )
-                      ],
-                    ),
-                  );
-                });
+                          horizontalSpacing(16.w),
+                          Expanded(
+                            flex: 2,
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              mainAxisAlignment: MainAxisAlignment.spaceAround,
+                              children: [
+                                Text(
+                                  orderItem.product!.title!,
+                                  style: MyFonts.styleRegular400_12,
+                                ),
+                                Text(
+                                  '${context.translate(LangKeys.egp)} ${orderItem.product!.price.toString()}',
+                                  style: MyFonts.styleMedium500_12,
+                                ),
+                                Text(
+                                  '${context.translate(LangKeys.orderNumber)} ${order.orderNumber}',
+                                  style: MyFonts.styleRegular400_12
+                                      .copyWith(color: MyColors.grey),
+                                ),
+                                verticalSpacing(8.h),
+                                Align(
+                                  alignment: Alignment.centerLeft,
+                                  child: CurvedButton(
+                                    height: 30.h,
+                                    style: MyFonts.styleMedium500_13
+                                        .copyWith(color: MyColors.white),
+                                    color: MyColors.baseColor,
+                                    title: textButton,
+                                    onTap: () {},
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                    );
+                  }).toList(),
+                );
+              },
+            );
           case OrderError:
             final errorState = state as OrderError;
             return Center(
