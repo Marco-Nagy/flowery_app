e9501bbdfa5a8aeb7029cf17dfcb8014becdfef4
Marco Nagy <marconbishay@gmail.com>
2025-02-05 00:25:59 +0200

    + fix filter my orders as in progress in Case Tab on Active tab + fix filter my orders as completed in Case Tab on Completed tab

--- lib/features/orders/presentation/view_model/order_cubit.dart
+++ lib/features/orders/presentation/view_model/order_cubit.dart
@@ -1,5 +1,6 @@
 import 'package:bloc/bloc.dart';
 import 'package:flowery_e_commerce/features/orders/domain/entities/order_response_entity.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:injectable/injectable.dart';
 import 'package:meta/meta.dart';
 
@@ -10,30 +11,36 @@ import '../../domain/use_cases/order_use_case.dart';
 import 'order_action.dart';
 
 part 'order_state.dart';
+
 @injectable
 class OrderCubit extends Cubit<OrderState> {
   final OrderUseCase _orderUseCase;
   @factoryMethod
+  List<Orders> orders = [];
 
   OrderCubit(this._orderUseCase) : super(OrderInitial());
 
   void doAction(OrderAction action) {
     switch (action) {
       case GetOrders():
-        _getAUserOrders();
+        _getAUserOrders(action);
     }
   }
 
-  Future<void> _getAUserOrders() async {
+  Future<void> _getAUserOrders(GetOrders action) async {
     emit(OrderLoading());
     final result = await _orderUseCase.getOrders();
     switch (result) {
       case Success<OrderResponseEntity>():
-        emit(OrderSuccess(
-            orders: result.data.orders ?? []));
+        orders.clear();
+        orders.addAll(
+          result.data.orders!.where(
+            (element) => element.state!.contains(action.orderState),
+          ),
+        );
+        emit(OrderSuccess(orders: orders));
       case Fail<OrderResponseEntity>():
-        emit(OrderError(
-            error: ErrorHandler.handle(result.exception!)));
+        emit(OrderError(error: ErrorHandler.handle(result.exception!)));
     }
   }
 }
