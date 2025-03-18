6190a47d39345ec6bc049ad8cade68748ed84c65
Marco Nagy <marconbishay@gmail.com>
2025-03-18 16:47:18 +0200

    feat: Refactor and improve notification handling and fix dependencies
    This commit includes several changes to enhance notification handling and address dependency issues:
    
    -   **Updated Gradle Java Home:** Changed the `org.gradle.java.home` path in `gradle.properties` to `C:\\Program Files\\Java\\jdk-17` for compatibility with the Windows environment.
    -   **Improved Checkout Logic:**
        -   Replaced `MessagingHelper` with `NotificationHelper` for topic subscription in `checkout_view_model_cubit.dart`.
        - This commit refactors notification handling during the checkout process by replacing `MessagingHelper` with `NotificationHelper` for topic subscriptions. This change ensures consistency and proper functionality within the notification system.
    -   **Dependency Injection Adjustments:**
        - Removed unnecessary imports in `di.config.dart`.
        - Replaced deprecated `regestet_context_module.dart` with proper dependency management.
        - Removed unused dependencies like `awesome_notifications`.
    -   **Refactored Routes:**
        -   Improved `app_routes.dart` to handle track order route errors gracefully.
        -   Added a loader as the default page to prevent issues.
        - Changed the way of getting  navigatorKey and put it in flowery_ecommerce class to be easily to use it.
    -   **Enhanced Main Initialization:**
        -   Updated `main.dart` to use `async` and `await` for initializing notifications.
        - fixed error with initialize notifications.
    -   **Notification Navigation:**
        -  Changed the navigation helper class to user navigatorKey from flowery_ecommerce class.
        -   Refactored `notification_navigation_helper.dart` to handle different notification types and navigation scenarios more efficiently.
    -   **Local Notification Service:**
        -   Revised `local_notification_service.dart` to use the new notification channel, handle tap actions correctly, and display notifications with payload data.
        - Removed  `regestet_context_module.dart`.
    -   **Corrected Code Generation:** Fixed code generation issues in multiple `*.g.dart` files.
    -  **Modified Custom Map** fix custom map design
    -  **Update firebase helper:** Changed firebase helper to notification helper
    - **Removed Awesome notifications** Removed  awesome_notifications from dependencies.
    - **fix order list:** fix design of order list.

--- lib/features/checkout/presentation/viewModel/checkout_view_model_cubit.dart
+++ lib/features/checkout/presentation/viewModel/checkout_view_model_cubit.dart
@@ -2,7 +2,7 @@ import 'package:bloc/bloc.dart';
 import 'package:flowery_e_commerce/core/networking/common/api_result.dart';
 import 'package:flowery_e_commerce/core/networking/error/error_handler.dart';
 import 'package:flowery_e_commerce/core/networking/error/error_model.dart';
-import 'package:flowery_e_commerce/core/services/firebase_notification/messaging_helper.dart';
+import 'package:flowery_e_commerce/core/services/firebase_notification/notification_helper.dart';
 import 'package:flowery_e_commerce/features/address/domain/entities/response/SavedAddressResponseEntity.dart';
 import 'package:flowery_e_commerce/features/checkout/domain/entities/cash_place_order_entity.dart';
 import 'package:flowery_e_commerce/features/checkout/domain/entities/credit_place_order_entity.dart';
@@ -58,7 +58,7 @@ class CheckoutViewModelCubit extends Cubit<CheckoutViewModelState> {
     var result = await _cashUseCase(currentAddress);
     switch(result) {
       case Success<CashPlaceOrderEntity>():
-        MessagingHelper().subscribeToTopic(result.data.orderId);
+        NotificationHelper().subscribeToTopic(result.data.orderId);
         emit(CheckoutCashSuccess(placeOrder:result.data));
       case Fail<CashPlaceOrderEntity>():
         emit(CheckoutError(error: ErrorHandler.handle(result.exception!)));
@@ -71,7 +71,7 @@ class CheckoutViewModelCubit extends Cubit<CheckoutViewModelState> {
     var result = await _creditUseCase(currentAddress);
     switch(result) {
       case Success<CreditPlaceOrderEntity>():
-        MessagingHelper().subscribeToTopic(result.data.orderId);
+        NotificationHelper().subscribeToTopic(result.data.orderId);
         emit(CheckoutCreditSuccess(placeOrder:result.data));
       case Fail<CreditPlaceOrderEntity>():
         emit(CheckoutError(error: ErrorHandler.handle(result.exception!)));
