6c3f2912c07f92d7d5f463b82c9a97a2e26d2a71
Marco Nagy <marconbishay@gmail.com>
2025-04-11 15:59:04 +0200

    feat: Update track order map screen and refactor app routes
    - Refactored the track order map screen to handle null data for order and driver.
    - Added navigation to order details from the map screen.
    - Adjusted camera view to fit markers or default to a specific location when data is missing.
    - Updated `main.dart` to log errors using Firebase Crashlytics.
    - Added a new dependency to track order map route.
    - Handled missing `orderId` and `userId` in `trackOrder` and `trackOrderMap` routes, to show app loader.
    - Updated  marker in map screen, to show empty set of markers when data is missing.
    - Updated  polylines in map screen, to show empty set of polylines when data is missing.
    - Update the camera animate in map screen, to adjust camera bounds or defualt bounds when data is missing.

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,6 @@
+import 'dart:async'; // Import Completer
+import 'dart:ui';
+
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flowery_store/core/services/firebase_notification/local_notification_service.dart';
@@ -12,11 +15,8 @@ import 'core/utils/abb_bloc_observer.dart';
 import 'di/di.dart';
 import 'firebase_options.dart';
 
-import 'dart:async'; // Import Completer
-
-
 // ✅ Completer to track initialization state
- final Completer<bool> appInitialized = Completer<bool>();
+final Completer<bool> appInitialized = Completer<bool>();
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -25,20 +25,24 @@ Future<void> main() async {
   Bloc.observer = MyBlocObserver();
 
   await dotenv.load(fileName: '.env.firebase');
+
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform)
       .whenComplete(() async {
-   await NotificationHelper().initialize();
+    await NotificationHelper().initialize();
     await LocalNotificationService.setupLocalNotifications();
-
-
   });
 
-  configureDependencies();
+  await FirebaseCrashlytics.instance.setCrashlyticsCollectionEnabled(true);
 
-  // ✅ Mark initialization as complete
+  // ✅ تسجيل الأخطاء
+  FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterFatalError;
+  PlatformDispatcher.instance.onError = (error, stack) {
+    FirebaseCrashlytics.instance.recordError(error, stack, fatal: true);
+    return true;
+  };
+
+  configureDependencies();
   appInitialized.complete(true);
 
-  FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterFatalError;
   runApp(FloweryStore());
 }
-
