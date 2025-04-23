fb2c9dd2097e14f3f02277aacf5acacc9f7ab982
Marco Nagy <marconbishay@gmail.com>
2025-04-23 02:39:41 +0200

    refactor: relocate `FirebaseServerToken` to `firebase_notification` directory
    The `FirebaseServerToken` file was moved from the root directory to the `firebase_notification` directory to improve code organization. The import statement was adjusted accordingly in `notification_helper.dart`.

--- lib/core/networking/api/api_manager.g.dart
+++ lib/core/networking/api/api_manager.g.dart
@@ -6,7 +6,7 @@ part of 'api_manager.dart';
 // RetrofitGenerator
 // **************************************************************************
 
-// ignore_for_file: unnecessary_brace_in_string_interps,no_leading_underscores_for_local_identifiers,unused_element,unnecessary_string_interpolations
+// ignore_for_file: unnecessary_brace_in_string_interps,no_leading_underscores_for_local_identifiers,unused_element,unnecessary_string_interpolations,unused_element_parameter
 
 class _ApiManager implements ApiManager {
   _ApiManager(this._dio, {this.baseUrl, this.errorLogger}) {
@@ -15,10 +15,8 @@ class _ApiManager implements ApiManager {
 
   final Dio _dio;
 
-  // ignore: unused_element_parameter
   String? baseUrl;
 
-  // ignore: unused_element_parameter
   final ParseErrorLogger? errorLogger;
 
   @override
